66f04ed0e47c0f404d8db106de17791108b706af
unknown <ctx145@DEV-PC-0133.bcs.dev>
2014-05-25 19:54:06 +0800

    turotial 3

--- perhapsgeek/polls/admin.py
+++ perhapsgeek/polls/admin.py
@@ -14,7 +14,7 @@ class ChoiceInline(admin.TabularInline):
 class PollAdmin(admin.ModelAdmin):
     fieldsets=[
                   (None,{'fields':['question']} ),
-                  ('Date information',{'fields':['pub_date']}),
+                  ('Date information',{'fields':['pub_date'],'classes':['collapse']}),
                ]
     inlines=[ChoiceInline]
     list_display=('question','pub_date','was_published_recently')
