3292f709f078a8c9646b2a0ffe62d319521c5eca
lu18887 <luyongning268@gmail.com>
2014-05-27 12:36:37 +0800

    tutorial 3 part 2

--- perhapsgeek/polls/views.py
+++ perhapsgeek/polls/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http    import Http404
+from django.http  import HttpResponse
+from polls.models import Poll
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello,world.You're at the poll index")
+    latest_poll_list=Poll.objects.order_by('-pub_date')[:5]
+    context={'latest_poll_list':latest_poll_list}
+    return render(request,'polls/index.html',context)
     
 def detail(request,poll_id):
-    return HttpResponse("You're looking at  poll %s." % poll_id)
+    try:
+        poll=Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request,'polls/detail.html',{'poll':poll})
 
 def results(request,poll_id):
     return HttpResponse("You're looking at the results of  poll %s." % poll_id)
