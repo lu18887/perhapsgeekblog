92e3ca20b7d59ed0770ad92dd2b67ba2433f9723
lu18887 <luyongning268@gmail.com>
2014-06-06 17:53:27 +0800

    turtorial 5 & 6

--- perhapsgeek/polls/models.py
+++ perhapsgeek/polls/models.py
@@ -13,7 +13,8 @@ class Poll(models.Model):
     def __unicode__(self):		#Pythong 3  :def __str__(self):
         return self.question
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now=timezone.now()
+        return now>self.pub_date and  self.pub_date>=now - datetime.timedelta(days=1)
     was_published_recently.admin_order_field='pub_date'
     was_published_recently.boolean=True
     was_published_recently.short_description='Published recently?'
